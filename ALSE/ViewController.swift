e523fb5f9ed211ead405ad0d3d55eb0a1e80b73c
Nick <nickgeorge32@gmail.com>
2020-06-02 00:01:28 +0200

    Initial Commit

---
+++ ALSE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALSE
+//
+//  Created by Nick George on 6/2/20.
+//  Copyright © 2020 SimpleDev Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
